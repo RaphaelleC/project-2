65cbf697ee894b9914b9ca178cb17aad4e4582f7
Antoinette Demonceaux <antoinettedemonceaux@hotmail.fr>
2021-05-10 12:14:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Browser as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './common/Home'
+import Nav from'./common/Nav'
+
+
+function App() {
+  return <h1>Hello World</h1>
+}
+
+export default App
---
+++ src/lib/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const baseUrl = 'https://ghibliapi.herokuapp.com/'
+
+export function getAllFilms() {
+  return axios.get(`${baseUrl}/films`)
+}
+
+export function getSingleFilm(filmId) {
+  return axios.get(`${baseUrl}/films/${filmId}`)
+}
+export function getAllPeople() {
+  return axios.get(`${baseUrl}/people`)
+}
+
+export function getSinglePerson() {
+  return axios.get(`${baseUrl}/people/${peopleId}`)
+}
+
